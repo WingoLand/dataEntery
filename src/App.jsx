52ded57927fc0a1ac01dbd0a29fdeb9b8012eefa
abdullah0saeed <abdullahhamouda056@gmail.com>
2024-08-17 01:46:46 +0300

    add delete database

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 
 import AddAlphabet from "./pages/AddAlphabet";
 import AddWord from "./pages/AddWord";
+import config from "../config";
+
+const { BASE_URL } = config;
 
 function App() {
   const [page, setPage] = useState("addAlphabet");
@@ -27,6 +30,41 @@ function App() {
       </div>
       {page === "addAlphabet" && <AddAlphabet />}
       {page === "addWords" && <AddWord />}
+      <div
+        style={{ marginTop: 100, display: "flex", justifyContent: "center" }}
+      >
+        <button
+          style={{
+            backgroundColor: "pink",
+            borderColor: "red",
+            width: 300,
+            height: 50,
+            fontSize: 20,
+            fontWeight: "bold",
+          }}
+          onClick={async () => {
+            const confirmDelete = window.confirm(
+              "Are you sure you want to delete the database?"
+            );
+
+            if (confirmDelete) {
+              try {
+                await fetch(`${BASE_URL}/deleteDB`, {
+                  method: "DELETE",
+                })
+                  .then((res) => res.json())
+                  .then((data) => {
+                    alert(data.message);
+                  });
+              } catch (error) {
+                alert("Error deleting database: " + error.message);
+              }
+            }
+          }}
+        >
+          Delete Database
+        </button>
+      </div>
     </>
   );
 }
